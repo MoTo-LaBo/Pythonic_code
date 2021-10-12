e1a700cfee8b410c72a197340e10ec596cb33a04
MoTo <moto.labo.desgin@gmail.com>
2021-10-12 17:26:53 +0900

    修正

--- basic/list_comprehension/02_c_f-conversion.py
+++ basic/list_comprehension/02_c_f-conversion.py
@@ -49,20 +49,24 @@
 
 """
 
+print('\n')
+print('------------ 摂氏 -> 華氏 interactive 完成 -------------')
+
+
 while True:
-    celsius_num = input('今日の気温を入力して下さい >>> ')
+    celsius_num = input('\n今日の気温を入力して下さい >>> ')
     # 入力された値(string) が数値かどうか判定　ー＞ int(integer)型へ
     if celsius_num.isdecimal():
         c_num = int(celsius_num)
 
         # 判定後の処理　-> 正常な入力値か？(正常な気温か？の振り分け)　-20°C ~ 40°C の一般的な領域を指定
         match c_num:
-            case int() if c_num >= -20 and c_num < 40:
-                print(f'華氏 -> {1.8 * c_num + 32:.3g} °Fです')
+            case int() if c_num >= -20 and c_num <= 40:
+                print(f'\n華氏 -> {1.8 * c_num + 32:.3g} °Fです')
                 break
             # -20°C ~ 40°C の領域を外れた場合の処理, その他例外全て (半角でなかった場合も含む)
             case _:
-                print(f'入力 Error です\n -20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
+                print(f'\n入力 Error です\n -20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
                 continue
 
     # 数字以外だった場合 & float(小数)の判定 -> float(小数)だった場合の処理
@@ -70,18 +74,18 @@ while True:
         try:
             c_fnum = float(celsius_num)
             match c_fnum:
-                case float() if c_fnum >= -20 and c_fnum < 40:
-                    print(f'華氏 : {1.8 * c_fnum + 32:.3g} °Fです')
+                case float() if c_fnum >= -20 and c_fnum <= 40:
+                    print(f'\n華氏 -> {1.8 * c_fnum + 32:.3g} °Fです')
                     break
                 # -20°C ~ 40°C の領域を外れた場合の処理, その他例外全て (半角でなかった場合も含む)
                 case _:
-                    print(f'入力 Error です\n -20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
+                    print(f'\n入力 Error です\n -20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
                     continue
         except ValueError:
-            print(f'入力された値は{celsius_num}です。\n数字の 20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
+            print(f'\n入力された値は >> {celsius_num} << です。\n数字の 20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
             continue
 
     # その他の場合の処理
     else:
-        print(f'入力された値は{celsius_num}です。\n数字の 20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
+        print(f'\n入力された値は >> {celsius_num} << です。\n数字の 20 ~ 40 or -20.0 ~ 40.0 の間で入力して下さい')
         continue
--- basic/list_comprehension/03_natural_num.py
+++ basic/list_comprehension/03_natural_num.py
@@ -1,6 +1,7 @@
 
 # ------------- 指定自然数 -> 倍数list生成 interactive 完成------------- #
 
+
 """
             1 ~ 100までの自然数を入力する
 
@@ -9,8 +10,12 @@
 """
 
 
+print('\n')
+print('------- 課題 ②：指定自然数の100以下の倍数一覧生成 --------')
+
+
 while True:
-    natural_num = input('1~100以下の自然数を１つ入力して下さい >>> ')
+    natural_num = input('\n1~100以下の自然数を１つ入力して下さい >>> ')
     # 入力された値(string) が数値かどうか判定　ー＞ int(integer)型へ
     if natural_num.isdecimal():
         n_num = int(natural_num)
@@ -19,24 +24,24 @@ while True:
         match n_num:
             case int() if n_num >= 1 and n_num <= 100:
                 n_list = [i for i in range(n_num, 100, n_num)]
-                print(f'自然数の倍数list : {n_list}')
+                print(f'\n自然数の倍数list : {n_list}')
                 break
             # 1 ~ 100 の領域を外れた場合の処理, その他例外全て (半角でなかった場合も含む)
             case _:
-                print(f'入力 Error です\n 1~100以下の自然数ので入力して下さい')
+                print(f'\n入力 Error です\n 1~100以下の自然数ので入力して下さい')
                 continue
 
     # 数字以外だった場合 & float(小数)の判定 -> float(小数)だった場合の処理
     elif not natural_num.isdecimal():
         try:
             n_fnum = float(natural_num)
-            print(f'入力された値は{n_fnum}です。\n 1~100以下の自然数ので入力して下さい')
+            print(f'\n入力された値は >> {n_fnum} << です。\n 1~100以下の自然数ので入力して下さい')
             continue
         except ValueError:
-            print(f'入力された値は{natural_num}です。\n数字の 1~100以下の自然数ので入力して下さい')
+            print(f'\n入力された値は >> {natural_num} << です。\n数字の 1~100以下の自然数ので入力して下さい')
             continue
 
     # その他の場合の処理
     else:
-        print(f'入力された値は{natural_num}です。\n数字の 1~100以下の自然数ので入力して下さい')
+        print(f'\n入力された値は >> {natural_num} << です。\n数字の 1~100以下の自然数ので入力して下さい')
         continue
